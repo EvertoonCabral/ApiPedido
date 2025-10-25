a998cdae10b34b5653287e8f57ef9978b6c00d7b
Everton <evertoncabral.dev1@gmail.com>
2025-10-25 02:20:47 -0300

    feat: organizado o swagger

---
+++ src/Infra/Api.Pedidos.Infra/Migrations/20251025045543_AjustesModelo.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Pedidos.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class AjustesModelo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Endereco_Id",
+                table: "Clientes");
+
+            migrationBuilder.RenameColumn(
+                name: "Endereco_Logradouro",
+                table: "Clientes",
+                newName: "Endereco_Rua");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Endereco_Rua",
+                table: "Clientes",
+                newName: "Endereco_Logradouro");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Endereco_Id",
+                table: "Clientes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
